b1eff8643a6f1ec70503de2ba510bd3566cd2f50
cya98 <todaudcksgk@naver.com>
2022-12-10 18:54:01 +0900

    darkmode

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,13 @@
 import { SessionProvider } from 'next-auth/react'
 import '../styles/globals.css'
+import { ThemeProvider } from 'next-themes'
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
-      <Component {...pageProps} />
+      <ThemeProvider attribute="class">
+        <Component {...pageProps} />
+      </ThemeProvider>
     </SessionProvider>
   )
 }
