39796747a079034b68c18d04b59a184b366fe03a
jeongseockhyeon <jeong9260@naver.com>
2022-12-10 18:15:59 +0900

    first commit

--- pages/api/chat/socketio.ts
+++ pages/api/chat/socketio.ts
@@ -1,24 +1,23 @@
-import { NextApiRequest } from 'next'
-import { NextApiResponseServerIO } from '../../../types/next'
-import { Server as ServerIO } from 'socket.io'
-import { Server as NetServer } from 'http'
+import React from 'react'
+import { Server } from 'socket.io'
+import messageHandler from '../../../utils/messageHandler'
 
-export const config = {
-  api: {
-    bodyParser: false,
-  },
-}
-
-export default async (req: NextApiRequest, res: NextApiResponseServerIO) => {
-  if (!res.socket.server.io) {
-    console.log('New Socket.io server...')
-    // adapt Next's net Server to http Server
-    const httpServer: NetServer = res.socket.server as any
-    const io = new ServerIO(httpServer, {
-      path: '/api/socketio',
-    })
-    // append SocketIO server to Next.js socket server response
-    res.socket.server.io = io
+export default function SocketHandler(req, res) {
+  if (res.socket.server.io) {
+    console.log('Already set up')
+    res.end()
+    return
   }
+
+  const io = new Server(res.socket.server)
+  res.socket.server.io = io
+
+  const onConnection = (socket) => {
+    messageHandler(io, socket)
+  }
+
+  io.on('conection', onConnection)
+
+  console.log('Setting up socket')
   res.end()
 }
